9340e83736479ca573780bc0c40180ebbf1a32cc
sohonroy <sohonroy@yahoo.com>
2014-04-25 17:37:26 +0200

    Popup Message 1.0
    Implemented basic popup display functionality (as demonstrated in the
    BumbleBee setup earlier)
    Note: Does not address concurrency of several simultaneous popups. Add
    In will hang.

--- AragornAddIn/AragornAddIn/ThisAddIn.cs
+++ AragornAddIn/AragornAddIn/ThisAddIn.cs
@@ -16,6 +16,9 @@ namespace AragornAddIn
 {
     public partial class ThisAddIn
     {
+
+        Excel.Shape textbox;
+        System.Timers.Timer popupDelay;
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
             //IAnalyzer a = new DefaultAnalyzer();
@@ -27,11 +30,53 @@ namespace AragornAddIn
 
         public void TurnOnAragorn()
         {
-            MessageBox.Show("Ready to roll!");
+            //MessageBox.Show("Ready to roll!");
+            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)Application.ActiveSheet); //select active worksheet
+            Excel.Range R = ((Excel.Range)Application.Selection); // points to the active selected cell or range
+
+            if (R.Top - 70 <= 0)
+            {
+                if (R.Left - 140 <= 0)
+                {
+                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, R.Left + R.Width, R.Top, 140, 90);
+
+                }
+                else
+                {
+                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, R.Left - 140, R.Top, 140, 90);
+
+                }
+            }
+            else
+            {
+                if (R.Left - 140 <= 0)
+                {
+                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, R.Left + R.Width, R.Top - 70, 140, 90);
+
+                }
+                else
+                {
+                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, R.Left - 140, R.Top - 70, 140, 90);
+
+                }
+            }
+
+            textbox.TextEffect.Text = "Beware! This cell is being used in formulas contained in cells " + R.DirectDependents.get_Address(false);
+            textbox.Fill.ForeColor.RGB = 0x87CEEB;
+
+            popupDelay = new System.Timers.Timer(3000);
+            popupDelay.Start();
+            popupDelay.Elapsed += new ElapsedEventHandler(VanishPopup);
+            
         }
 
 
+        private void VanishPopup(object source, ElapsedEventArgs e)
+        {
 
+            textbox.Delete();
+            popupDelay.Stop();
+        }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
