e63634a48d355789568bf2296f15ce680d4e3aad
sohonroy <sohonroy@yahoo.com>
2014-04-29 14:21:34 +0200

    Trying SelectionChange Event
    Trial with Sheet.SelectionChange Event.
    Problem: Target.DirectDependents trigger selection change event causing
    infinte loop.

--- AragornAddIn/AragornAddIn/ThisAddIn.cs
+++ AragornAddIn/AragornAddIn/ThisAddIn.cs
@@ -22,6 +22,7 @@ namespace AragornAddIn
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
             //IAnalyzer a = new DefaultAnalyzer();
+            //Excel.Worksheet activeWorksheet1 = ((Excel.Worksheet)Application.ActiveSheet); //select active worksheet
             
 
             
@@ -31,43 +32,57 @@ namespace AragornAddIn
         public void TurnOnAragorn()
         {
             //MessageBox.Show("Ready to roll!");
-            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)Application.ActiveSheet); //select active worksheet
-            Excel.Range R = ((Excel.Range)Application.Selection); // points to the active selected cell or range
+            Excel.Worksheet activeWorksheet1 = ((Excel.Worksheet)Application.ActiveSheet); //select active worksheet
+            //Excel.Range R = ((Excel.Range)Application.Selection); // points to the active selected cell or range
 
-            if (R.Top - 70 <= 0)
+            activeWorksheet1.SelectionChange += new  Excel.DocEvents_SelectionChangeEventHandler(activeWorksheet1_SelectionChange);
+
+           
+            
+        }
+
+
+
+        void activeWorksheet1_SelectionChange(Excel.Range Target)
+        {
+
+            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)Application.ActiveSheet); //select active worksheet
+            //Excel.Range R = ((Excel.Range)Application.Selection); // points to the active selected cell or range
+            MessageBox.Show(" :O :O");
+            if (Target.Top - 70 <= 0)
             {
-                if (R.Left - 140 <= 0)
+                if (Target.Left - 140 <= 0)
                 {
-                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, R.Left + R.Width, R.Top, 140, 90);
+                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, Target.Left + Target.Width, Target.Top, 140, 90);
 
                 }
                 else
                 {
-                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, R.Left - 140, R.Top, 140, 90);
+                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, Target.Left - 140, Target.Top, 140, 90);
 
                 }
             }
             else
             {
-                if (R.Left - 140 <= 0)
+                if (Target.Left - 140 <= 0)
                 {
-                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, R.Left + R.Width, R.Top - 70, 140, 90);
+                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, Target.Left + Target.Width, Target.Top - 70, 140, 90);
 
                 }
                 else
                 {
-                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, R.Left - 140, R.Top - 70, 140, 90);
+                    textbox = activeWorksheet.Shapes.AddTextbox(Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal, Target.Left - 140, Target.Top - 70, 140, 90);
 
                 }
             }
 
-            textbox.TextEffect.Text = "Beware! This cell is being used in formulas contained in cells " + R.DirectDependents.get_Address(false);
+            textbox.TextEffect.Text = "Beware! This cell is being used in formulas contained in cells ";//+ Target.DirectDependents.get_Address(false);
             textbox.Fill.ForeColor.RGB = 0x87CEEB;
 
             popupDelay = new System.Timers.Timer(3000);
             popupDelay.Start();
             popupDelay.Elapsed += new ElapsedEventHandler(VanishPopup);
-            
+            //throw new NotImplementedException();
         }
 
 
